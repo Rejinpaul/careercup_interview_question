211f741fe7f8fed4ed0d5a7bb2309dc726120ea8
Boyang Xu <xu5bo5yang@gmail.com>
2015-04-13 13:10:30 -0400

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,7 +2,8 @@
 *	Description:
 *	Give you an array which has n integers,it has both positive and negative integers.Now you need sort this array in a special way.After that,the negative integers should in the front,and the positive integers should in the back.Also the relative position should not be changed. 
 *	eg. -1 1 3 -2 2 
-*	ans: -1 -2 1 3 2. 
+*	ans: -1 -2 1 3 2
+*  http://www.careercup.com/question?id=5201559730257920
 */
 
 #include <stdio.h>
